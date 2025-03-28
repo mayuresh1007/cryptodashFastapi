cdc0e641d8c97e14d089e68ff1d71304c7ec6309
Mayuresh <kmayuresh7@gmail.com>
2025-03-28 14:02:35 +0530

    Update

--- app/models.py
+++ app/models.py
@@ -9,3 +9,7 @@ class UpdateUser(BaseModel):
     new_email: Optional[EmailStr] = None
     new_password: Optional[str] = None
     wishlist: Optional[List[str]] = None
+
+class LoginRequest(BaseModel):
+    email: str
+    password: str
